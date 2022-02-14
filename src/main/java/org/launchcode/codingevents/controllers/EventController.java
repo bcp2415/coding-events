4c5f3062959ad3a2b1768e3cf4b7cd90a86f8f1e
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-13 19:27:56 -0600

    finishes regular exercises for chapter 11

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,15 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
-    private static List<String> events = new ArrayList<>();
+    private Event event = new Event();
+
+    private static ArrayList<Event> events = new ArrayList<Event>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        Event apple = new Event();
+        apple.setName("Baked Appel");
+        apple.setDescription("Come hear how Apple renders its devices useless at a date they determine.");
+        Event meta = new Event();
+        meta.setName("Meta Meta");
+        meta.setDescription("Pre Meta Epi Anti In En Ana...How many prepositions strung together can you make sense of?");
+        Event loop = new Event();
+        loop.setName("Loops in the Loop");
+        loop.setDescription("Have we lost control of control structures?");
+        events.add(apple);
+        events.add(meta);
+        events.add(loop);
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,7 +43,7 @@ public class EventController {
     // lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+        //events.put(eventName);
         return "redirect:";
     }
 }
