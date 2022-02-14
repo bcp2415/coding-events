7d06e9848e7c06ff9fc44f3b4615a40ae9a6c5a2
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-14 15:13:04 -0600

    codes along with video 1 in ch 12, nearly at end

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,45 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        ArrayList<Event> events = new ArrayList<Event>();
-        Event apple = new Event();
-        apple.setName("Baked Apples");
-        apple.setDescription("Come hear how Apple renders its devices useless at a date they determine.");
-        apple.setHref("coffee");
-        Event meta = new Event();
-        meta.setName("Meta Meta");
-        meta.setDescription("Pre Meta Epi Anti In En Ana...How many prepositions strung together can you make sense of?");
-        meta.setHref("monastery");
-        Event loop = new Event();
-        loop.setName("Loops in the Loop");
-        loop.setDescription("Have we lost control of control structures?");
-        loop.setHref("horse");
-        events.add(apple);
-        events.add(meta);
-        events.add(loop);
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        //events.put(eventName);
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(new Event(eventName));
         return "redirect:";
     }
 }
