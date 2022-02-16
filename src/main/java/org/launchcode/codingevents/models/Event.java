22af3c9f7769589c0c026b185eebc4d808c7a14b
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2022-02-16 11:20:09 -0600

    adds equals and hashCode overrides to Event

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
 
     private int id;
@@ -40,4 +42,16 @@ public class Event {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
